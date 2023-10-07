264d5a8851ec32e596c011cc11768c96eeb7032c
Reece Lim <lim.reecesergei@gmail.com>
2023-10-07 12:01:15 +0800

    feat: add page header with design

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,12 +3,13 @@ import '../styles/SignIn.css'
 
 function SignIn() {
   return (
-      <>
-        <div>
-          <SignInForm/>
+      <div className="sign-in-container">
+        <SignInForm />
+        <div className="page-header">
+          <h1>THE BEST DIGITAL WALLET IN THE WORLD</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id vestibulum ex. </p>
         </div>
-        
-      </>
+      </div>
     );
 }
 
