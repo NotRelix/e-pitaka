49290138c13701d2402beb5cdb81a43ff2444191
Reece Lim <lim.reecesergei@gmail.com>
2023-10-07 13:37:28 +0800

    feat: add sign in icons

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,16 +1,35 @@
 import SignInForm from "../components/SignInForm";
+import DownArrow from "../assets/down_arrow.svg"
+import Money from "../assets/money.png"
+import Send from "../assets/send.png"
+import Wallet from "../assets/wallet.png"
 import '../styles/SignIn.css'
 
 function SignIn() {
   return (
-      <div className="sign-in-container">
-        <SignInForm />
-        <div className="page-header">
-          <h1>THE BEST DIGITAL WALLET IN THE WORLD</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id vestibulum ex. </p>
+    <div className="sign-in-container">
+      <SignInForm />
+      <div className="page-header">
+        <h1>THE BEST DIGITAL WALLET IN THE WORLD</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id vestibulum ex. </p>
+        <img src={DownArrow} className="align-self-center" alt="" />
+        <div className="sign-in-icons">
+          <div className="sign-in-icon">
+            <img src={Money} alt="" />
+            <label>MONEY</label>
+          </div>
+          <div className="sign-in-icon">
+            <img src={Send} alt="" />
+            <label>SEND</label>
+          </div>
+          <div className="sign-in-icon">
+            <img src={Wallet} alt="" />
+            <label>WALLET</label>
+          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default SignIn;
