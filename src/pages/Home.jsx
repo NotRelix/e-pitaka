8aaa33d5defb62008950ee62940214f35ae52d12
Oni-sama-web <57718480+Oni-sama-web@users.noreply.github.com>
2023-10-08 13:34:50 +0000

    Check the display pls

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,67 +1,49 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import "../styles/Home.css"
 
 function Home() {
     return (
-        <>
-            <div>
-                <div className="current_box">
-                    <div className="current_inside">
-                        <div>
-                            <h2>CURRENT BALANCE: </h2><b/>
-                            <p className="money_text">₱ 1,000,000.00</p>
-                        </div>
-                        <div>
-                            <button className="inside_hide">
-                                <img className="img_hide"src="./src/assets/View_hide.png"></img>
-                            </button>
-                        </div>
-                    </div>
-                    
-                </div>
-                <footer>
-                    <div className="foot_style">
-                        <div>
-                            <center>
-                                <button className="contain_box">
-                                    <img src="./src/assets/Money.png" alt="Money"></img>
-                                    <h2>DEPOSIT</h2>
-                                </button>
-                            </center>
-
-                        </div>
-                        <div>
-                            <center>
-                                <button className="contain_box">
-                                    <img src="./src/assets/Send_fill.png" alt="Send_fill"></img>
-                                    <h2>SEND</h2>
-                                </button>
-                            </center>
-                        </div>
-                        <div>
-                            <center>
-                                <button className="contain_box">
-                                    <img src="./src/assets/Ticket_alt.png" alt="Ticket_alt"></img>
-                         <h2>HISTORY</h2>
-                                </button>
-                            </center>
-                        </div>
-                        <div>
-                            <center>
-                                <button className="contain_box">
-                                    <img src="./src/assets/Wallet.png" alt="Wallet"></img>
-                                    <h2>SAVINGS</h2>
-                                </button>
-                                
-                            </center>
-                        </div>
-                    </div>
-                </footer>
+      <>
+        <div>
+          <div className="current_box">
+            <div className="current_inside">
+              <div>
+                <h2 className="home_text">CURRENT BALANCE: </h2>
+                <p className="money_text">₱ 1,000,000.00</p>
+              </div>
+              <div>
+                <button className="inside_hide">
+                  <img className="img_hide" src="./src/assets/View_hide.png" alt="View Hide" />
+                </button>
+              </div>
             </div>
-            
-           
-        </>
-    )
-}
-
-export default Home
+          </div>
+          <div>
+            <footer className="footer_style">
+                <div className="foot_style">
+                <button className="contain_box">
+                    <img className="image_home" src="./src/assets/Money.png" alt="Money" />
+                    <h2 className="home_text">DEPOSIT</h2>
+                </button>
+                <button className="contain_box">
+                    <img className="image_home" src="./src/assets/Send_fill.png" alt="Send Fill" />
+                    <h2 className="home_text">SEND</h2>
+                </button>
+                <button className="contain_box">
+                    <img className="image_home" src="./src/assets/Ticket_alt.png" alt="Ticket Alt" />
+                    <h2 className="home_text">HISTORY</h2>
+                </button>
+                <button className="contain_box">
+                    <img className="image_home" src="./src/assets/Wallet.png" alt="Wallet" />
+                    <h2 className="home_text">SAVINGS</h2>
+                </button>
+                </div>
+            </footer>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default Home;
+  
