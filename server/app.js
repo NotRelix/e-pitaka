4e9e5170095988d874ac04ce40e617e019a279a3
Reece Lim <lim.reecesergei@gmail.com>
2023-11-26 21:59:16 +0800

    feat: add connection to database

--- server/app.js
+++ server/app.js
@@ -4,6 +4,17 @@ const port = 3000
 const sql = require('mysql2')
 app.use(express.json())
 
+const conn = sql.createConnection({
+  host: 'localhost',
+  password: '',
+  user: 'root',
+  database: 'epitaka-db'
+})
+
+app.post('/sign-up', (req, res) => {
+  
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
