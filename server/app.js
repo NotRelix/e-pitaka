b5cc6e8bd349a6b691877926091fc63f88457e4c
Reece Lim <lim.reecesergei@gmail.com>
2023-11-26 21:32:10 +0800

    nit: change email to username and add mysql2

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const sql = require('mysql2')
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
