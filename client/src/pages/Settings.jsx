90f7a0827dc0ecd08dbf3c55b0b0901e90d0d768
Oni-sama <hayatominmin@gmail.com>
2023-12-04 17:15:16 +0800

    Missing Notification Popup still working on it

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {useState} from 'react';
 import "../styles/Settings.css";
 import closeButton from "../assets/close_ring_light.png";
+import ChangePass from "../Popup/ChangePass";
+import AccountRecovery from "../Popup/AccountRecovery";
 
 function Settings() {
+  const [CPbutton, setCPbutton] = useState(false);
+  const [ARbutton, setARbutton] = useState(false);
   const navigate = useNavigate();
 
   const handleCloseClick = () => {
@@ -23,11 +28,11 @@ function Settings() {
           />
         </div>
         <div className="settings-body">
-          <Link onClick="" className="setting-option">
+          <Link onClick={()=> setCPbutton(true)} className="setting-option">
             <img src="../src/assets/Key_alt.png" alt="Change Password" />
             <h4>CHANGE PASSWORD</h4>
           </Link>
-          <Link onClick="" className="setting-option">
+          <Link onClick={()=> setARbutton(true)} className="setting-option">
             <img src="../src/assets/User_fill.png" alt="Account Recovery" />
             <h4>SET <br/> ACCOUNT RECOVERY</h4>
           </Link>
@@ -35,6 +40,9 @@ function Settings() {
             <img src="../src/assets/Bell_pin.png" alt="Notification Settings" />
             <h4>NOTIFICATION <br/> SETTINGS</h4>
           </Link>
+
+          <ChangePass trigger={CPbutton} setTrigger={setCPbutton}/>
+          <AccountRecovery trigger={ARbutton} setTrigger={setARbutton}/>
         </div>
       </div>
     </>
