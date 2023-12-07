a0b00ef6d308403b5af3f691d0ffaee2073df1da
Roxanne Lopez <roxanneaggielopez@gmail.com>
2023-12-07 21:09:45 +0800

    fix: display deleted users

--- client/src/components/HistoryPopup.jsx
+++ client/src/components/HistoryPopup.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import "../styles/HistoryPopup.css";
 import userprofile from "../assets/user_profile.png";
 import closeButton from "../assets/Close_round_light.png";
 
 function HistoryPopup({ username, popUpInfo, showPopup, setShowPopup }) {
+  const navigate = useNavigate();
+
   return showPopup ? (
     <>
       <div className="popup">
@@ -27,7 +30,7 @@ function HistoryPopup({ username, popUpInfo, showPopup, setShowPopup }) {
                     account on {new Date(popUpInfo.Date).toLocaleString()}
                   </p>
                   <hr />
-                  <p>For inquiries and concerns, click this link</p>
+                  <p>For inquiries and concerns, click this <a onClick={() => navigate("/e-pitaka/help")}>link</a></p>
                 </div>
               ) : (
                 <div>
@@ -46,15 +49,15 @@ function HistoryPopup({ username, popUpInfo, showPopup, setShowPopup }) {
                 <div>
                   <img src={userprofile} className="user-profile" />
                   <h3 className="user-fullname">
-                    {popUpInfo.Destination_fname} {popUpInfo.Destination_lname}
+                    {popUpInfo.Destination_fname ?? "deleted user"} {popUpInfo.Destination_lname ?? ""}
                   </h3>
                   <small className="user-name">
-                    {popUpInfo.Destination_username}
+                    {popUpInfo.Destination_username ?? "deleted user"}
                   </small>
                   <h3 className="amount">₱ {popUpInfo.Amount}</h3>
                   <p>
                     You successfully sent ₱ {popUpInfo.Amount} to{" "}
-                    {popUpInfo.Destination_fname} {popUpInfo.Destination_lname}{" "}
+                    {popUpInfo.Destination_fname  ?? "deleted user"} {popUpInfo.Destination_lname  ?? ""}{" "}
                     on {new Date(popUpInfo.Date).toLocaleString()}
                   </p>
                   <hr />
@@ -66,15 +69,15 @@ function HistoryPopup({ username, popUpInfo, showPopup, setShowPopup }) {
                 <div>
                   <img src={userprofile} className="user-profile" />
                   <h3 className="user-fullname">
-                    {popUpInfo.Source_fname} {popUpInfo.Source_lname}
+                    {popUpInfo.Source_fname  ?? "deleted user"} {popUpInfo.Source_lname  ?? ""}
                   </h3>
                   <small className="user-name">
-                    {popUpInfo.Source_username}
+                    {popUpInfo.Source_username ?? "deleted user"}
                   </small>
                   <h3 className="amount">₱ {popUpInfo.Amount}</h3>
                   <p>
                     You successfully received ₱ {popUpInfo.Amount} from{" "}
-                    {popUpInfo.Source_fname} {popUpInfo.Source_lname} on{" "}
+                    {popUpInfo.Source_fname ?? "deleted user"} {popUpInfo.Source_lname ?? ""} on{" "}
                     {new Date(popUpInfo.Date).toLocaleString()}
                   </p>
                   <hr />
